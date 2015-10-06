bb1cf745aa4464c763825981229940e255e3019d
Piotr Walkow <piotr.walkow@reiss.com>
2015-10-06 16:30:57 +0100

    PromotionBundle: actions -> benefits

--- src/Sylius/Component/Core/spec/Promotion/Benefit/FixedDiscountBenefitSpec.php
+++ src/Sylius/Component/Core/spec/Promotion/Benefit/FixedDiscountBenefitSpec.php
@@ -9,20 +9,22 @@
  * file that was distributed with this source code.
  */
 
-namespace spec\Sylius\Component\Core\Promotion\Action;
+namespace spec\Sylius\Component\Core\Promotion\Benefit;
 
 use PhpSpec\ObjectBehavior;
 use Sylius\Component\Core\Model\AdjustmentInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Promotion\Benefit\FixedDiscountBenefit;
 use Sylius\Component\Originator\Originator\OriginatorInterface;
 use Sylius\Component\Promotion\Model\PromotionInterface;
+use Sylius\Component\Promotion\Benefit\PromotionBenefitInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 /**
  * @author Paweł Jędrzejewski <pawel@sylius.org>
  * @author Saša Stamenković <umpirsky@gmail.com>
  */
-class PercentageDiscountActionSpec extends ObjectBehavior
+class FixedDiscountBenefitSpec extends ObjectBehavior
 {
     function let(RepositoryInterface $adjustmentRepository, OriginatorInterface $originator)
     {
@@ -31,34 +33,32 @@ class PercentageDiscountActionSpec extends ObjectBehavior
 
     function it_is_initializable()
     {
-        $this->shouldHaveType('Sylius\Component\Core\Promotion\Action\PercentageDiscountAction');
+        $this->shouldHaveType(FixedDiscountBenefit::class);
     }
 
     function it_implements_Sylius_promotion_action_interface()
     {
-        $this->shouldImplement('Sylius\Component\Promotion\Action\PromotionActionInterface');
+        $this->shouldImplement(PromotionBenefitInterface::class);
     }
 
-    function it_applies_percentage_discount_as_promotion_adjustment(
+    function it_applies_fixed_discount_as_promotion_adjustment(
         $adjustmentRepository,
         $originator,
         OrderInterface $order,
         AdjustmentInterface $adjustment,
         PromotionInterface $promotion
     ) {
-        $order->getPromotionSubjectTotal()->willReturn(10000);
         $adjustmentRepository->createNew()->willReturn($adjustment);
         $promotion->getDescription()->willReturn('promotion description');
 
-        $adjustment->setAmount(-2500)->shouldBeCalled();
+        $adjustment->setAmount(-500)->shouldBeCalled();
         $adjustment->setLabel(AdjustmentInterface::PROMOTION_ADJUSTMENT)->shouldBeCalled();
         $adjustment->setDescription('promotion description')->shouldBeCalled();
 
         $originator->setOrigin($adjustment, $promotion)->shouldBeCalled();
 
         $order->addAdjustment($adjustment)->shouldBeCalled();
-
-        $configuration = array('percentage' => 0.25);
+        $configuration = array('amount' => 500);
 
         $this->execute($order, $configuration, $promotion);
     }
